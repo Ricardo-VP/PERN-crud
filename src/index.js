c7492dd322052466d2830405b85d761f45a7b44d
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-12-10 00:46:36 -0500

    Cambiando puerto

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(morgan('dev'));
 
-app.listen(3000)
+app.listen(4000)
 
-console.log('Server is running on port 3000');
+console.log('Server is running on port 4000');
