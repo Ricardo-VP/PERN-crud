19a6dd57d0acb017e7686b2541b083f440b91324
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-12-26 01:38:25 -0500

    Agregando metodo post

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,3 +1,5 @@
+const pool = require("../db");
+
 const getAllTasks = async (req, res) => {
   res.send("retrieving all tasks");
 };
@@ -6,8 +8,20 @@ const getTask = (req, res) => {
   res.send("Retrieving a single task");
 };
 
-const createTask = (req, res) => {
-  res.send("Creating a task");
+const createTask = async (req, res) => {
+  const { title, description } = req.body;
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *",
+      [title, description]
+    );
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+    res.json({ error: error.message });
+  }
 };
 
 const deleteTask = (req, res) => {
