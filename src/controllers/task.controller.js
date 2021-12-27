e92b0563fad1f94c04ea9110ab1a0b0a2ebe6481
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-12-26 20:02:03 -0500

    Agregando metodo getTask

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -9,8 +9,22 @@ const getAllTasks = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.send("Retrieving a single task");
+const getTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await pool.query("SELECT * FROM task WHERE id = $1", [id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 const createTask = async (req, res) => {
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -13,7 +13,7 @@ const router = Router();
 
 router.get("/tasks", getAllTasks);
 
-router.get("/tasks/10", getTask);
+router.get("/tasks/:id", getTask);
 
 router.post("/tasks", createTask);
 
