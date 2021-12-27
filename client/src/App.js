cfa34cff4a0d7725ab90d061c0eec1aa9900bde0
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-12-26 21:38:32 -0500

    Agregando navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
+import Menu from './components/Navbar';
+import { Container } from "@mui/material";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<TaskList />} />
-        <Route path="/tasks/new" element={<TaskForm />} />
-        {/* Falta ruta editar */}
-      </Routes>
+      <Menu />
+      <Container>
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/tasks/new" element={<TaskForm />} />
+          {/* Falta ruta editar */}
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,37 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function Navbar() {
-    return (
-        <div>
-            Nav bar
-        </div>
-    )
+  const navigate = useNavigate();
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="transparent">
+        <Container>
+          <Toolbar>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              <Link style={{ textDecoration: "none", color: "#eee" }} to="/">
+                PERN Stack
+              </Link>
+            </Typography>
+
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => navigate("/tasks/new")}
+            >
+              New TASK
+            </Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
+  );
 }
