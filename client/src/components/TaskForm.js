bc3cf62bac87b476ae2c16f1723ffa36b0edc3b6
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-12-26 21:57:12 -0500

    Agregando formulario de creacion

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -1,7 +1,81 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState, useEffect } from "react";
+
 export default function TaskForm() {
-    return (
-        <div>
-            Task Form
-        </div>
-    )
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Evita que se recargue la página
+    console.log(task);
+  };
+
+  const handleChange = (e) => {
+    setTask({ ...task, [e.target.name]: e.target.value }); // Actualiza el estado
+  };
+
+  return (
+    <Grid
+      direction="column"
+      container
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Grid item xs={3}>
+        <Card
+          sx={{ mt: 5 }}
+          style={{
+            backgroundColor: "#1e272e",
+            padding: "1rem",
+          }}
+        >
+          <Typography variant="5" textAlign="center" color="white">
+            Create Task
+          </Typography>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <TextField
+                variant="filled"
+                label="Write your title"
+                sx={{
+                  display: "block",
+                  margin: ".5rem 0",
+                }}
+                name="title"
+                onChange={handleChange}
+                inputProps={{ style: { color: "#eee" } }}
+                InputLabelProps={{ style: { color: "#eee" } }}
+              />
+              <TextField
+                variant="filled"
+                label="Write your description"
+                multiline
+                rows={4}
+                sx={{
+                  display: "block",
+                  margin: ".5rem 0",
+                }}
+                name="description"
+                onChange={handleChange}
+                inputProps={{ style: { color: "#eee" } }}
+                InputLabelProps={{ style: { color: "#eee" } }}
+              />
+              <Button variant="contained" color="primary" type="submit">
+                Save
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 }
