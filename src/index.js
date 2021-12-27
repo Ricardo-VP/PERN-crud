f94abd6bc6deb3c6f15dccdadba28f1b803ec744
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-12-26 20:31:30 -0500

    Usando modulo cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const taskRoutes = require("./routes/tasks.routes");
 
 const app = express();
+
+app.use(cors());
 
 app.use(morgan("dev"));
 
