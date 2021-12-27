5c5a8e66dd93d6e9dc82111c931afb92e0b91c20
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-12-26 20:30:47 -0500

    Agregando variable db a config archivo

--- src/db.js
+++ src/db.js
@@ -1,12 +1,12 @@
 const { Pool } = require("pg");
-require('dotenv').config();
+const { db } = require("./config");
 
 const pool = new Pool({
-  user: "postgres",
-  password: process.env.DB_PASSWORD,
-  host: "localhost",
-  port: 5432,
-  database: "taskdb",
+  user: db.user,
+  password: db.password,
+  host: db.host,
+  port: db.port,
+  database: db.database,
 });
 
 module.exports = pool;
