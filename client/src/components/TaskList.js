9a7ddd5f17c08859abaadfd8b8f5c953f95ad87e
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-12-26 22:23:09 -0500

    Creando el componente TaskList

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,7 +1,56 @@
+import { Button, Card, CardContent, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+
 export default function TaskList() {
-    return (
-        <div>
-            Task List
-        </div>
-    )
+  const [tasks, setTasks] = useState([]);
+
+  const loadTasks = async () => {
+    const res = await fetch("http://localhost:4000/tasks");
+    const data = await res.json();
+    setTasks(data);
+  };
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  return (
+    <>
+      <h1>Task List</h1>
+      {tasks.map((task) => (
+        <Card
+          style={{
+            marginBottom: ".7rem",
+            backgroundColor: "#1e272e",
+          }}
+        >
+          <CardContent
+            style={{ display: "flex", justifyContent: "space-between" }}
+          >
+            <div style={{ color: 'white' }}>
+              <Typography>{task.title}</Typography>
+              <Typography>{task.description}</Typography>
+            </div>
+            <div>
+              <Button
+                variant="contained"
+                color="inherit"
+                onClick={() => console.log("edit")}
+              >
+                Edit
+              </Button>
+              <Button
+                variant="contained"
+                color="warning"
+                onClick={() => console.log("delete")}
+                style={{ marginLeft: ".5rem" }}
+              >
+                Delete
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      ))}
+    </>
+  );
 }
